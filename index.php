33b5ba42dfafe11a5574e5e73dad5ff5f40676ab
Marcos Lopes <90800648+Marcosl2@users.noreply.github.com>
2023-04-19 09:22:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,6 +8,7 @@
 </head>
 <pre>
 <body>
+
     <?php
 
     require_once 'ContaBanco.php';
